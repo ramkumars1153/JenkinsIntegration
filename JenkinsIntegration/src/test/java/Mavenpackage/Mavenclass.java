046fc09381e4570f2c94bd0963c0e2ef25b11f9a
ramkumars1153 <ramkumars1153@gmail.com>
2022-02-06 15:20:24 +0530

    Pom changes to Git

--- JenkinsIntegration/src/test/java/Mavenpackage/Mavenclass.java
+++ JenkinsIntegration/src/test/java/Mavenpackage/Mavenclass.java
@@ -1,8 +1,11 @@
 package Mavenpackage;
 
+import org.testng.annotations.Test;
+
 public class Mavenclass
 {
-	public static void main(String[] args)
+	@Test
+	public void TestNg()
 	{
 		System.out.println("God Is Great");
 	}
